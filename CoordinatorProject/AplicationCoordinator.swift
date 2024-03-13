f3f20b96b0b09f2ea3344ee5691213d2da42d8db
Daniil Kulikovskiy <danilkulikovskiy@gmail.com>
2024-03-13 22:36:19 +0300

    add onboardingState

--- CoordinatorProject/AplicationCoordinator.swift
+++ CoordinatorProject/AplicationCoordinator.swift
@@ -7,27 +7,51 @@
 
 import UIKit
 import SwiftUI
+import Combine
 
 final class AplicationCoordinator: Coordinator {
     
     var window: UIWindow
     var childCoordinator = [Coordinator]()
     
+    let hasSeenOnboarding = CurrentValueSubject<Bool, Never>(false)
+    var cancellable = Set<AnyCancellable>()
+    
     init(window: UIWindow) {
         self.window = window
     }
     
     func start() {
-//        let onboardingCoordinator = OnboardingCoordinator()
-//        onboardingCoordinator.start()
-//        self.childCoordinator = [onboardingCoordinator]
-//        window.rootViewController = onboardingCoordinator.rootViewController
+        setupOnboardingValue()
         
-        let mainCoordinator = MainCoordinator()
-        mainCoordinator.start()
-        self.childCoordinator = [mainCoordinator]
-        window.rootViewController = mainCoordinator.rootViewController
+        hasSeenOnboarding
+            .removeDuplicates()
+            .sink { [weak self] hasSeen in
+                if hasSeen {
+                    let mainCoordinator = MainCoordinator()
+                    mainCoordinator.start()
+                    self?.childCoordinator = [mainCoordinator]
+                    self?.window.rootViewController = mainCoordinator.rootViewController
+                } else if let hasSeenOnboarding = self?.hasSeenOnboarding {
+                    let onboardingCoordinator = OnboardingCoordinator(hasSeenOnboarding: hasSeenOnboarding)
+                    onboardingCoordinator.start()
+                    self?.childCoordinator = [onboardingCoordinator]
+                    self?.window.rootViewController = onboardingCoordinator.rootViewController
+                }
+            }
+            .store(in: &cancellable)
     }
     
-    
+    func setupOnboardingValue() {
+        let key = "hasSeenOnboarding"
+        let value = UserDefaults.standard.bool(forKey: key)
+        hasSeenOnboarding.send(value)
+        
+        hasSeenOnboarding
+            .filter{ $0 == true }
+            .sink { value in
+                UserDefaults.standard.setValue(value, forKey: key)
+            }
+            .store(in: &cancellable)
+    }
 }
--- CoordinatorProject/Onboarding/OnboardingCoordinator.swift
+++ CoordinatorProject/Onboarding/OnboardingCoordinator.swift
@@ -7,15 +7,22 @@
 
 import Foundation
 import SwiftUI
+import Combine
+
 
 final class OnboardingCoordinator: Coordinator {
     
     var rootViewController = UIViewController()
+    var hasSeenOnboarding: CurrentValueSubject<Bool, Never>
+    
+    init(hasSeenOnboarding: CurrentValueSubject<Bool, Never>) {
+        self.hasSeenOnboarding = hasSeenOnboarding
+    }
     
     func start() {
         
-        let view = OnboardingView {
-            
+        let view = OnboardingView { [weak self] in
+            self?.hasSeenOnboarding.send(true)
         }
         
         rootViewController = UIHostingController(rootView: view)
